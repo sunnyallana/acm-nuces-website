b1aa5c3e4b6886232cbbc3ac98cb9e5e88c94507
sunnyallana <study.sunnyallana@gmail.com>
2024-09-29 19:32:54 +0500

    Cards for excom

--- src/pages/OurTeam.jsx
+++ src/pages/OurTeam.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import FacultyList from '../components/TeamPageComponents/FacultyList';
+import ExecutiveCommittee from '../components/TeamPageComponents/ExecutiveCommittee';
 
 export function OurTeam() {
   return (
     <>
       <FacultyList/>
+      <ExecutiveCommittee/>
     </>
   );
 }
