13ddf8fe152568e21492a6be5c7531db7c0b727f
sunnyallana <study.sunnyallana@gmail.com>
2024-10-01 20:56:11 +0500

    Updated logo and loaders size

--- src/components/GlobalComponents/LoaderComponent.jsx
+++ src/components/GlobalComponents/LoaderComponent.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import logo from "../../assets/acm-logo-png.png"; // Ensure the path to the logo is correct
+import logo from "../../assets/acm.svg"; // Ensure the path to the SVG logo is correct
 
 const Loader = () => {
   return (
     <div className="flex-col gap-4 w-full flex items-center justify-center min-h-screen">
       <div className="relative flex items-center justify-center">
         {/* Outer rotating circle */}
-        <div className="w-40 h-40 border-4 border-transparent text-blue-400 animate-spin flex items-center justify-center border-t-[#0b466d] rounded-full">
+        <div className="w-64 h-64 border-4 border-transparent text-blue-400 animate-spin flex items-center justify-center border-t-[#0b466d] rounded-full">
           {/* Inner rotating circle */}
-          <div className="w-36 h-36 border-4 border-transparent text-red-400 animate-spin flex items-center justify-center border-t-[#257fb5] rounded-full"></div>
+          <div className="w-56 h-56 border-4 border-transparent text-red-400 animate-spin flex items-center justify-center border-t-[#257fb5] rounded-full"></div>
         </div>
 
         {/* Static image in the center */}
-        <div className="absolute flex items-center justify-center w-40 h-40">
-          <img src={logo} alt="Logo" className="w-32 h-32" /> {/* Increased size for the logo */}
+        <div className="absolute flex items-center justify-center w-64 h-64">
+          <img src={logo} alt="Logo" className="w-48 h-48" /> {/* Increased size for the logo */}
         </div>
       </div>
     </div>
